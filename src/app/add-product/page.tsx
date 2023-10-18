4608529ebcdc0b4d74d3ac3ee8066e2282c0a18a
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2023-10-18 12:56:12 -0700

    add: add-product page

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,10 @@
+export default function AddProductPage() {
+    return (
+        <div>
+            <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+            <form>
+                <input name="name" placeholder="Name" className="input input-bordered w-full mb-3" />
+            </form>
+        </div>
+    )
+}
