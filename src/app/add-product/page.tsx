46d98e7e694e454d8c6d7879ae105a491502e6e8
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2023-10-20 19:55:51 -0700

    add: addProduct sends new product to mongoDB server

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,9 +1,40 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import prisma from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+    title: "Add Product"
+}
+
+async function addProduct(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name")?.toString();
+    const description = formData.get("description")?.toString();
+    const imageUrl = formData.get("imageUrl")?.toString();
+    const price = Number(formData.get("price") || 0);
+
+    if (!name || !description || !imageUrl || !price) {
+        throw Error("Missing required fields");
+    }
+
+    await prisma.product.create({
+        data: { name, description, imageUrl, price },
+    });
+    
+    redirect("/");
+}
+
 export default function AddProductPage() {
     return (
         <div>
             <h1 className="text-lg mb-3 font-bold">Add Product</h1>
-            <form>
-                <input name="name" placeholder="Name" className="input input-bordered w-full mb-3" />
+            <form action={addProduct}>
+                <input required name="name" placeholder="Name" className="input input-bordered w-full mb-3" />
+                <textarea required name="description" placeholder="Description" className="textarea-bordered textarea mb-3 w-full"/>
+                <input required name="imageUrl" placeholder="Image URL" type="url" className="input input-bordered w-full mb-3" />
+                <input required name="price" placeholder="Price" type="number" className="input input-bordered w-full mb-3" />
+                <FormSubmitButton type="submit" className="btn-block">Add Product</FormSubmitButton>
             </form>
         </div>
     )
---
+++ src/components/FormSubmitButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ComponentProps } from "react";
+
+type FormSubmitButtonProps = {
+    children: React.ReactNode,
+    className?: string,
+} & ComponentProps<"button">
+
+export default function FormSubmitButton(
+    {children, className} : FormSubmitButtonProps
+) {
+    return (
+        <button
+            className={`btn btn-primary ${className}`}
+        >
+            {children}
+        </button>
+    )
+}
